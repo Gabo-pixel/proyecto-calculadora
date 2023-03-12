d220ce8452001765cfbf16dab041931dc314447d
gabriel <gabriel.garces727@gmail.com>
2023-03-12 13:49:37 -0500

    proyecto inicial

---
+++ script.js
@@ -0,0 +1,21 @@
+alert("esta es la megacalculadora");
+const suma = document.querySelector('#suma')
+suma.addEventListener('mouseover' , function() {
+    this.style.backgroundColor = blue;
+});
+
+miDiv.addEventListener('mouseout', function() {
+    this.style.backgroundColor = '';
+  });
+
+
+  const suma = document.querySelector('#resta')
+  resta.addEventListener('mouseover' , function() {
+      this.style.backgroundColor = 'blue';
+  });
+  
+  miDiv.addEventListener('mouseout', function() {
+      this.style.backgroundColor = '';
+    });
+
+  
